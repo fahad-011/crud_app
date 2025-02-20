aa2d646371a748b168d86e6ac612992978e8586a
roc102 <mdfahadrahman1969@gmail.com>
2025-02-20 10:33:25 +0530

    added db auth

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("Welcome!");
+  res.status(302).json("Welcome!");
 });
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`server is listening to ${PORT}`);
+  console.log(`server listening to port ${PORT}`);
 });
